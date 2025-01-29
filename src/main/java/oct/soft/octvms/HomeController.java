dc323f57a2b50f7122e7fa64e8266c19eb70063b
osantau <octavian.santau.os@gmail.com>
2025-01-29 22:38:55 +0200

    Solve Language Switch problem.
    Add bootstrap modal form. To fix modal window.

--- src/main/java/oct/soft/octvms/HomeController.java
+++ src/main/java/oct/soft/octvms/HomeController.java
@@ -1,14 +1,31 @@
 package oct.soft.octvms;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import oct.soft.octvms.visitor.model.VisitorDTO;
 
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request, Model model) {
+    	String lang = request.getParameter("lang");
+    	model.addAttribute("showChoose","true");
+    	model.addAttribute("visitor", new VisitorDTO());
+    	if(lang!=null)
+    	{
+    		model.addAttribute("showChoose","false");
+    		model.addAttribute("showRegForm","true");
+    	} else {
+    		model.addAttribute("showRegForm","false");
+    		model.addAttribute("showChoose","true");
+    	}
         return "home/index";
     }
 
